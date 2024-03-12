96ae35aa7f708690b68a8f8c94aece6e01700041
Tecnologia1982Babita <90773090+Tecnologia1982Babita@users.noreply.github.com>
2024-03-12 12:28:42 -0300

    Add files via upload

---
+++ precache-manifest.351728ca46d92f725af86ae00169f4a9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29fa4e3000908d67484013ec094634b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ce9879093120b005554",
+    "url": "/static/css/2.de0652d2.chunk.css"
+  },
+  {
+    "revision": "71e7bee6e7be18b335e9",
+    "url": "/static/css/main.1da11a71.chunk.css"
+  },
+  {
+    "revision": "3ce9879093120b005554",
+    "url": "/static/js/2.c3ab7b8c.chunk.js"
+  },
+  {
+    "revision": "f2bb71794654715be0cfd5829039e696",
+    "url": "/static/js/2.c3ab7b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a386f5062a2d20b8e54",
+    "url": "/static/js/3.abd3ef0f.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/3.abd3ef0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6728afd8938b257cdd09",
+    "url": "/static/js/4.88baea9a.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/static/js/4.88baea9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ff0b80df97efc12014",
+    "url": "/static/js/5.3edde584.chunk.js"
+  },
+  {
+    "revision": "41cd032238f0cddd8734de1b3133877a",
+    "url": "/static/js/5.3edde584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e7bee6e7be18b335e9",
+    "url": "/static/js/main.4cde6d53.chunk.js"
+  },
+  {
+    "revision": "8517d27b3da2b1c36915",
+    "url": "/static/js/runtime-main.907b009a.js"
+  }
+]);
