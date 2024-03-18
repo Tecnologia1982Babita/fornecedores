ddb179d0d7276fb760e1f686884d8db82b897000
Tecnologia1982Babita <90773090+Tecnologia1982Babita@users.noreply.github.com>
2024-03-18 10:05:08 -0300

    Add files via upload

---
+++ precache-manifest.da0d00da2822d756ea5d627b7955e47b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c189306644bcd29792ad8177f0ac6a0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3cf76eb74f816ae1f1bb",
+    "url": "./static/css/2.de0652d2.chunk.css"
+  },
+  {
+    "revision": "d51e8589866dabb95057",
+    "url": "./static/css/main.1da11a71.chunk.css"
+  },
+  {
+    "revision": "3cf76eb74f816ae1f1bb",
+    "url": "./static/js/2.de018b7d.chunk.js"
+  },
+  {
+    "revision": "f2bb71794654715be0cfd5829039e696",
+    "url": "./static/js/2.de018b7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c42b64aa7866f5daab",
+    "url": "./static/js/3.ea10d03b.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "./static/js/3.ea10d03b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6728afd8938b257cdd09",
+    "url": "./static/js/4.88baea9a.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "./static/js/4.88baea9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c494844ab6ccc7f2996",
+    "url": "./static/js/5.a98927cf.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "./static/js/5.a98927cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51e8589866dabb95057",
+    "url": "./static/js/main.f6ea36fe.chunk.js"
+  },
+  {
+    "revision": "bebd4d9c48641c74c4bf",
+    "url": "./static/js/runtime-main.98118178.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.351728ca46d92f725af86ae00169f4a9.js"
+  "./precache-manifest.da0d00da2822d756ea5d627b7955e47b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
